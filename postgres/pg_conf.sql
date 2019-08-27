a27129a816a2538a6657076e2911d86cb7146f59
Yannic Nevado Hidalgo <yannic.hidalgo@stud.h-da.de>
2019-08-27 14:49:35 +0200

    Create pg_conf.sql

---
+++ postgres/pg_conf.sql
@@ -0,0 +1,16 @@
+-- Configure postgresql.conf
+
+-- Logging
+ALTER SYSTEM SET log_destination = 'stderr';
+ALTER SYSTEM SET logging_collector = 'on';
+ALTER SYSTEM SET log_directory = '/var/log/postgresql';
+ALTER SYSTEM SET log_filename = 'postgresql-%a.log';
+ALTER SYSTEM SET log_min_messages = 'info';
+ALTER SYSTEM SET log_checkpoints = 'on';
+ALTER SYSTEM SET log_connections = 'on';
+ALTER SYSTEM SET log_disconnections = 'on';
+ALTER SYSTEM SET log_truncate_on_rotation= 'on';
+ALTER SYSTEM SET log_rotation_age= '1d';
+ALTER SYSTEM SET log_rotation_size= '0';
+ALTER SYSTEM SET log_duration= 'on';
+ALTER SYSTEM SET log_line_prefix='%m [%p] ';
